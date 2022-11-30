3a91b8b44c983250d0b401710f56d0fe2513f668
Standa Geidl <standag@gmail.com>
2022-11-30 07:32:30 +0100

    prepare for day 1

---
+++ src/day01.hs
@@ -0,0 +1,17 @@
+module Day01 (solve_part_one, solve_part_two) where
+
+-- |
+-- Solve part one.
+--
+-- >>> solve_part_one
+--
+solve_part_one :: ()
+solve_part_one = ()
+
+-- |
+-- Solve part two.
+--
+-- >>> solve_part_two
+--
+solve_part_two :: ()
+solve_part_two = ()
